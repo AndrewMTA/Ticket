b41e2187d7f9462e2c5929fc4d95d588c551849a
AndrewMTA <2353andrew@gmail.com>
2023-08-31 09:46:51 -0500

    k

--- src/Cart.js
+++ src/Cart.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart, updateQuantity } from './cartSlice';
-import {Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
@@ -23,7 +24,11 @@ const Cart = () => {
   };
 
   const handleQuantityChange = (itemId, newQuantity) => {
-    dispatch(updateQuantity({ id: itemId, quantity: newQuantity }));
+    if (newQuantity === 0) {
+      dispatch(removeFromCart({ id: itemId }));
+    } else {
+      dispatch(updateQuantity({ id: itemId, quantity: newQuantity }));
+    }
   };
 
   const handleRemoveItem = (itemId) => {
@@ -31,40 +36,38 @@ const Cart = () => {
   };
 
   return (
-    <div style={{width: '100%', padding: ' 0px 35px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}className="cart-container">
+    <div style={{ width: '100%', padding: ' 0px 35px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }} className="cart-container">
       <h2>Cart</h2>
       {cartItems.length === 0 ? (
+
         <p>Your cart is empty.</p>
       ) : (
-        <table style={{width: '100%', textAlign: 'center'}}> 
-          <thead style={{backgroundColor:'gray'}}>
+        <table style={{ width: '100%', textAlign: 'center' }}>
+          <thead style={{ backgroundColor: 'gray' }}>
             <tr>
               <th>Item</th>
               <th>Quantity</th>
-          
               <th>Total Price</th>
               <th></th>
             </tr>
           </thead>
-          <tbody >
+          <tbody>
             {cartItems.map((item) => (
-              <tr style={{backgroundColor:'#e8e8e8', border:'1px solid blue'}} key={item.item.id}>
-                <td >{item.item.title}</td>
+              <tr style={{ backgroundColor: '#e8e8e8', border: '1px solid blue' }} key={item.item.id}>
+                <td>{item.item.title}</td>
                 <td>
-                  <button style={{margin:"0px 5px"}}onClick={() => handleQuantityChange(item.item.id, item.quantity - 1)}>-</button>
+                  <button style={{ margin: '0px 5px' }} onClick={() => handleQuantityChange(item.item.id, item.quantity - 1)}>-</button>
                   {item.quantity}
-                  <button style={{margin:"0px 5px"}}onClick={() => handleQuantityChange(item.item.id, item.quantity + 1)}>+</button>
+                  <button style={{ margin: '0px 5px' }} onClick={() => handleQuantityChange(item.item.id, item.quantity + 1)}>+</button>
                 </td>
-                
                 <td>${calculateItemTotal(item)}</td>
-            
                 <td>
                   <button onClick={() => handleRemoveItem(item.item.id)}>Remove</button>
                 </td>
               </tr>
             ))}
           </tbody>
-          <br/><br/>
+          <br /><br />
           <tfoot>
             <tr>
               <td colSpan="3">Total Items: {calculateTotalItems()}</td>
@@ -72,8 +75,8 @@ const Cart = () => {
             </tr>
           </tfoot>
         </table>
-      )}
-    <Link to="/Checkout">< button style={{marginTop:'-25px', position: 'absolute'}}>Checkout</button></Link>  
+      )}{cartItems.length !== 0 &&
+        <Link to="/Checkout"><button style={{ marginTop: '-25px', position: 'absolute' }}>Checkout</button></Link>}
     </div>
   );
 };
